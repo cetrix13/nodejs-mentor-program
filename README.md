408dba4e920b91d5c1fb4c1d194a4d310af1c16d
Alexandr Dadykin <dadykinav@yandex.ru>
2020-01-08 18:33:45 +0300

    Add server side validation

--- README.md
+++ README.md
@@ -20,12 +20,13 @@ Please, run the following command to check task 1.2.
 npm run task1.2
 ```
 
-### Task 2.1
+### Task 2.1 && Task 2.2
 
-Please, run the following command to check task 2.1.
+Please, run the following command to check task 2.1 and task 2.2
 ```
-npm run task2.1
+npm run task2
 ```
 - create user: http://localhost:3000/users (POST). Request example is in assets/create-user.json
 - view user: http://localhost:3000/users/1 (GET)
 - update user: http://localhost:3000/users/1 (PUT)
+- delete user: http://localhost:300/users/1 (PUT). Set flag `isDeleted` to false. 
--- src/task_2.js
+++ src/task_2.js
@@ -1,48 +1,52 @@
 import User from '../models/User';
+import { validateSchema } from './helpers';
 
 const express = require('express');
+const Joi = require('@hapi/joi');
 const app = express();
 const router = express.Router();
 const port = process.env.PORT;
 const users = [];
+
+const schema = Joi.object().keys({
+    id: Joi.string().required(),
+    login: Joi.string().required(),
+    password: Joi.string().alphanum().required(),
+    age: Joi.number().min(4).max(130).required(),
+    isDeleted: Joi.boolean().required()
+});
 
 router.route('/users/:id')
     .get((req, res) => {
         const { params: { id } } = req;
         const user = users.find(user => user.id === id);
         if (user) {
-            res.send(user);
+            res.status(200).send(user);
         } else {
-            res.statusCode = 404;
-            res.end();
+            res.status(404).end();
         }
     })
-    .post((req,res) => {
+    .post(validateSchema(schema),(req,res) => {
         const { id, login, password, age, isDeleted } = req.body;
         const user = new User(id, login, password, age, isDeleted);
         users.push(user);
 
         res.setHeader('Content-Type', 'application/json');
-        res.statusCode = 200;
-
-        res.send(user);
+        res.status(200).send(user);
     })
-    .put((req, res) => {
+    .put(validateSchema(schema), (req, res) => {
         const { params: { id } } = req;
         const index = users.findIndex(user => user.id === id);
 
         if (index !== -1) {
             users[index] = { ...users[index], ...req.body };
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 200;
-            res.send(users[index]);
+            res.status(200).json(users[index]);
         } else {
-            res.statusCode = 404;
-            res.end();
+            res.status(404).end();
         }
     });
 
 app.use(express.json());
 app.use('/', router);
 
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running on http://localhost:${port}/users`));
