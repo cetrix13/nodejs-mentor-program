a3944df3c77b895650b6673020b6a4bada5f7253
Alexandr Dadykin <dadykinav@yandex.ru>
2020-01-08 12:27:38 +0300

    Update instructions to task2.1, add example JSON

--- src/helpers.js
+++ src/helpers.js
@@ -2,8 +2,5 @@ export const reversedStr = input => (input.split('').reverse().join(''));
 
 export const uniqueID = (() => {
     let count  = 0;
-    return () => {
-        count++;
-        return count;
-    };
+    return () => ++count;
 })();
