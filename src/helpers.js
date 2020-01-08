ae9e64588235fc87fe3f3bc3e20aaa139baaf812
Alexandr Dadykin <dadykinav@yandex.ru>
2020-01-08 18:33:45 +0300

    Add server side validation

--- src/helpers.js
+++ src/helpers.js
@@ -4,3 +4,30 @@ export const uniqueID = (() => {
     let count  = 0;
     return () => ++count;
 })();
+
+
+export const errorResponse = (schemaErrors) => {
+    const errors = schemaErrors.map((error) => {
+        let { path, message } = error;
+        return { path, message };
+    });
+    return {
+        status: 'failed',
+        errors,
+    }
+};
+
+export function validateSchema(schema) {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body, {
+            aboartEarly: false,
+            allowUnknown: false,
+        });
+        
+        if (error && error.isJoi) {
+            res.status(400).json(errorResponse(error.details))
+        } else {
+            next();
+        }
+    }
+};
