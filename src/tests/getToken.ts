1b99cfb3320f431304dca5ece5ab13d64cb23cc5
Alexandr Dadykin <DadykinAV@yandex.ru>
2020-03-21 07:47:05 +0300

    Exclude node_modules in tsconfig

--- src/tests/getToken.ts
+++ src/tests/getToken.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import app from '../app';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+// @ts-ignore
 import authUser from '../../assets/auth-user.json';
 
 chai.use(chaiHttp);
